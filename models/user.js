c3b61e86ae9a4c26ee9357e4839ac5a42ac795f8
Sego87 <michelesegolini.developer@gmail.com>
2019-05-16 16:15:32 +0200

    password encryption

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const passportLocalMongoose = require('passport-local-mongoose');
+const mongooseBcrypt = require('mongoose-bcrypt');
 
 const userSchema = new mongoose.Schema({
     first_name: {
@@ -23,7 +24,8 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: 'Password is required'
+        required: 'Password is required',
+        bcrypt: true
     },
     isAdmin: {
         type: Boolean,
@@ -31,6 +33,7 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.plugin(mongooseBcrypt); // we need to add our userSchema to the plugin in order to use it. We can add the bcrypt option to our schema password field, we do this by adding bcrypt:true to the schema password field
 userSchema.plugin(passportLocalMongoose, { usernameField: 'email' }); // we need to add our userSchema to the plugin in order to use it. The second parameter is an option object, check the documentation on the internet (using 'email' we are saying that we want to use the entered email as username of the user-by default it would look for the field called username, which we don't have-)
 
 module.exports = mongoose.model('User',  userSchema);
